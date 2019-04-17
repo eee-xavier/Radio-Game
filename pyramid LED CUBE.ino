704b5ca4445aa53f259a2df10eb7121255098694
Matthew1998923 <49632839+Matthew1998923@users.noreply.github.com>
2019-04-17 13:28:22 +0800

    Update pyramid LED CUBE.ino

--- pyramid LED CUBE.ino
+++ pyramid LED CUBE.ino
@@ -20,21 +20,21 @@ byte RFID5[4]={0x04, 0xAA, 0xC3, 0x1A};
 byte RFID6[4]={0x04, 0x35, 0xDC, 0x1A};
 byte RFID7[4]={0x04, 0xB2, 0xC3, 0x1A};
 void setup() {
-	Serial.begin(9600);
-	SPI.begin();
-	mfrc522.PCD_Init();
-	for (byte i = 0; i < 6; i++) {
-		key.keyByte[i] = 0xFF;
-	}
-	pinMode(OUT1, OUTPUT);
-	pinMode(OUT2, OUTPUT);
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  pinMode(OUT1, OUTPUT);
+  pinMode(OUT2, OUTPUT);
   pinMode(OUT3, OUTPUT);
   pinMode(OUT4, OUTPUT);
 
   
   
-	digitalWrite(OUT1, LOW);
-	digitalWrite(OUT2, LOW);
+  digitalWrite(OUT1, LOW);
+  digitalWrite(OUT2, LOW);
   digitalWrite(OUT3, LOW);
   digitalWrite(OUT4, LOW);
 
@@ -44,7 +44,7 @@ void setup() {
 }
 
 void loop() {
-	
+  
     if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
         delay(50);
         if(millis()-last >200)
@@ -67,13 +67,13 @@ void loop() {
         Serial.print(' ');
     }
     Serial.println();
-	if (mfrc522.uid.uidByte[0] == RFID3[0] &&
-		mfrc522.uid.uidByte[1] == RFID3[1] &&
-		mfrc522.uid.uidByte[2] == RFID3[2] &&
-		mfrc522.uid.uidByte[3] == RFID3[3]
-		) {
-		digitalWrite(OUT1, HIGH);
-		digitalWrite(OUT2, LOW);
+  if (mfrc522.uid.uidByte[0] == RFID3[0] &&
+    mfrc522.uid.uidByte[1] == RFID3[1] &&
+    mfrc522.uid.uidByte[2] == RFID3[2] &&
+    mfrc522.uid.uidByte[3] == RFID3[3]
+    ) {
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
     digitalWrite(OUT3, LOW);
     digitalWrite(OUT4, LOW);
 
@@ -98,12 +98,100 @@ void loop() {
     digitalWrite(OUT2, LOW);
     digitalWrite(OUT3, LOW);
     digitalWrite(OUT4, HIGH);
-  
-  
-   
+
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
     delay(250);
    
-	}
+  }
  else if((mfrc522.uid.uidByte[0] == RFID2[0] &&
    mfrc522.uid.uidByte[1] == RFID2[1] &&
     mfrc522.uid.uidByte[2] == RFID2[2] &&
@@ -111,8 +199,130 @@ void loop() {
     (mfrc522.uid.uidByte[0] == RFID4[0] &&
    mfrc522.uid.uidByte[1] == RFID4[1] &&
     mfrc522.uid.uidByte[2] == RFID4[2] &&
-    mfrc522.uid.uidByte[3] == RFID4[3])||
-    (mfrc522.uid.uidByte[0] == RFID5[0] &&
+    mfrc522.uid.uidByte[3] == RFID4[3])) {
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(400);  
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    }
+  else if((mfrc522.uid.uidByte[0] == RFID5[0] &&
    mfrc522.uid.uidByte[1] == RFID5[1] &&
     mfrc522.uid.uidByte[2] == RFID5[2] &&
     mfrc522.uid.uidByte[3] == RFID5[3])||
@@ -123,32 +333,168 @@ void loop() {
     (mfrc522.uid.uidByte[0] == RFID7[0] &&
    mfrc522.uid.uidByte[1] == RFID7[1] &&
     mfrc522.uid.uidByte[2] == RFID7[2] &&
-    mfrc522.uid.uidByte[3] == RFID7[3])
-    ) {
+    mfrc522.uid.uidByte[3] == RFID7[3]))
+    {
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(400);
+    digitalWrite(OUT1, HIGH);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, HIGH);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(400);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
     digitalWrite(OUT1, LOW);
     digitalWrite(OUT2, HIGH);
     digitalWrite(OUT3, LOW);
     digitalWrite(OUT4, HIGH);
-   
-    
-    delay(400);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, HIGH);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, HIGH);
+    delay(200);
+    digitalWrite(OUT1, LOW);
+    digitalWrite(OUT2, LOW);
+    digitalWrite(OUT3, LOW);
+    digitalWrite(OUT4, LOW);
+    delay(200);
+    }
+  else
+  {
     digitalWrite(OUT1, LOW);
     digitalWrite(OUT2, LOW);
     digitalWrite(OUT3, LOW);
     digitalWrite(OUT4, LOW);
 
-  
-    delay(400);
-      
-    }
-	else
-	{
-		digitalWrite(OUT1, LOW);
-		digitalWrite(OUT2, LOW);
-    digitalWrite(OUT3, LOW);
-    digitalWrite(OUT4, LOW);
-
     
-	}
+  }
     delay(50);
 }
